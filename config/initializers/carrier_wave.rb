8a0e3df2780224a10ed68f08a675ba4f54c335a3
dahun <dahun4032@gmail.com>
2020-02-10 00:22:19 +0900

    [UPDATE] carrier wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
     config.fog_credentials = {
         # Amazon S3의 설정
         :provider              => 'AWS',
-        :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
+        :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
